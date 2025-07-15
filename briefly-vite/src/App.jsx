8a86d3a8cba29dd2f91b3b54e75efa017f24597c
Sayak Ghosh <gsayak22@gmail.com>
2025-07-16 00:47:34 +0530

    vite integration

---
+++ briefly-vite/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+export default function Popup() {
+  const handleClick = () => {
+    console.log('Record button clicked');
+    // navigator.mediaDevices.getUserMedia({ audio: true })
+    //   .then(stream => {
+    //     console.log('Microphone access granted.');
+    //   })
+    //   .catch(error => {
+    //     console.error('Microphone access denied:', error);
+    //   });
+  };
+
+  return (
+    <div className="popup">
+      <h1>Hello Extensions</h1>
+      <button onClick={handleClick}>Record</button>
+    </div>
+  );
+}
